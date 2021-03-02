b0e7f79a8a16bc79f174fc998f833304c2e33acc
ac15acf <36898098+ac15acf@users.noreply.github.com>
2021-03-02 22:47:35 +0000

    update weather icons

--- src/app/feature-components/new-weather/weather-app/weather-app.component.ts
+++ src/app/feature-components/new-weather/weather-app/weather-app.component.ts
@@ -13,16 +13,27 @@ export class WeatherAppComponent implements OnInit {
   constructor(private store: Store) { }
 
   private weatherData$: Observable<any>;
-  weatherData;
+  private pathToCustomIcons: string = "assets/custom-weather-icons/";
+  private svgSuffix: string = ".svg";
+  private defaultImage: string = "02d" + this.svgSuffix;
+  public weatherData;
   weatherSub: Subscription;
 
   ngOnInit(): void {
     this.weatherData$ = this.store.select(selectWeather);
     this.weatherSub = this.weatherData$.subscribe( response => {
       if(response){
+        console.log("response: ", response  ) 
         this.weatherData = response;
       }
     });
+  }
+
+  getWeatherIcon(weatherIconCode: string): string{
+    if(weatherIconCode){
+      return this.pathToCustomIcons + weatherIconCode + this.svgSuffix;
+    }
+    return this.pathToCustomIcons + this.defaultImage;
   }
 
 }
